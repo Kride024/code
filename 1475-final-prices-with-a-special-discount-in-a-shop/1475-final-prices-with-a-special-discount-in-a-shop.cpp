86e1de22f6113361eeee5256d8dcc1490e4b1739
Kriti yadav <156906180+Kride024@users.noreply.github.com>
2024-12-18 23:39:54 +0530

    Time: 0 ms (100.00%), Space: 14.3 MB (9.86%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -6,7 +6,7 @@ public:
         dsc.push(n-1);
         vector<int>res(n);
         res[n-1]=prices[n-1];
-        for(int i=n-2;i>=0;i--){
+        for(int i=n-2;i>=0;--i){
             while(!dsc.empty()and prices[dsc.top()]>prices[i])
                 dsc.pop();
             if(dsc.empty())res[i]=prices[i];
